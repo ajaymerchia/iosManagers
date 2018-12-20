9eab917f4bcbf87a49faba5cb23be6dcbb62d3bc
Ajay Merchia <ajaymerchia@Ajays-MBP.attlocal.net>
2018-12-19 18:13:24 -0800

    added other managers, not generalized yet

--- Classes/LayoutManager.swift
+++ Classes/LayoutManager.swift
@@ -8,29 +8,32 @@
 import Foundation
 import UIKit
 
-public class LayoutManager {
-    public static func belowCentered(elementAbove: UIView, padding: CGFloat, width:CGFloat, height: CGFloat) -> CGRect {
+class LayoutManager {
+    static func belowCentered(elementAbove: UIView, padding: CGFloat, width:CGFloat, height: CGFloat) -> CGRect {
         return CGRect(x: elementAbove.frame.midX - width/2, y: elementAbove.frame.maxY + padding, width: width, height: height)
     }
-    public static func belowLeft(elementAbove: UIView, padding: CGFloat, width:CGFloat, height: CGFloat) -> CGRect {
+    static func belowLeft(elementAbove: UIView, padding: CGFloat, width:CGFloat, height: CGFloat) -> CGRect {
         return CGRect(x: elementAbove.frame.minX, y: elementAbove.frame.maxY + padding, width: width, height: height)
     }
-    public static func belowRight(elementAbove: UIView, padding: CGFloat, width:CGFloat, height: CGFloat) -> CGRect {
+    static func belowRight(elementAbove: UIView, padding: CGFloat, width:CGFloat, height: CGFloat) -> CGRect {
         return CGRect(x: elementAbove.frame.maxX - width, y: elementAbove.frame.maxY + padding, width: width, height: height)
     }
     
-    public static func aboveCentered(elementBelow: UIView, padding: CGFloat, width:CGFloat, height: CGFloat) -> CGRect {
+    static func aboveCentered(elementBelow: UIView, padding: CGFloat, width:CGFloat, height: CGFloat) -> CGRect {
         return CGRect(x: elementBelow.frame.midX - width/2, y: elementBelow.frame.minY - (padding+height), width: width, height: height)
     }
-    public static func aboveLeft(elementBelow: UIView, padding: CGFloat, width:CGFloat, height: CGFloat) -> CGRect {
+    static func aboveLeft(elementBelow: UIView, padding: CGFloat, width:CGFloat, height: CGFloat) -> CGRect {
         return CGRect(x: elementBelow.frame.minX, y: elementBelow.frame.minY - (padding+height), width: width, height: height)
     }
     
-    public static func aboveRight(elementBelow: UIView, padding: CGFloat, width:CGFloat, height: CGFloat) -> CGRect {
+    static func aboveRight(elementBelow: UIView, padding: CGFloat, width:CGFloat, height: CGFloat) -> CGRect {
         return CGRect(x: elementBelow.frame.maxX - width, y: elementBelow.frame.minY - (padding+height), width: width, height: height)
     }
     
-    public static func between(elementAbove: UIView, elementBelow: UIView, width:CGFloat, topPadding: CGFloat, bottomPadding: CGFloat) -> CGRect {
+    static func between(elementAbove: UIView, elementBelow: UIView, width:CGFloat, topPadding: CGFloat, bottomPadding: CGFloat) -> CGRect {
+        
+        debugPrint(elementAbove.frame.maxY + topPadding)
+        
         return CGRect(x: elementAbove.frame.midX - width/2, y: elementAbove.frame.maxY + topPadding, width: width, height: elementBelow.frame.minY - (elementAbove.frame.maxY + topPadding + bottomPadding))
         
     }
